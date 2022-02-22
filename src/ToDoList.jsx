db67239513a7645535f35f8480fe8fd811eb7ffb
taniryla <ryland0000@gmail.com>
2022-02-22 11:49:49 -0700

    first commit

---
+++ src/ToDoList.jsx
@@ -0,0 +1,10 @@
+import ToDoListItem from "./ToDoListItem"
+
+export default function ToDoList(){
+    return (
+        <ul>
+            <ToDoListItem />
+            <ToDoListItem />
+        </ul>
+    );
+}
